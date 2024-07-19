da181240dad115b14c61cebe5717767b924c32e4
unknown <abhijan097@gmail.com>
2024-07-19 17:10:38 +0545

    ul

--- index.js
+++ index.js
@@ -18,16 +18,15 @@ Create a navbar in JSX:
 */
 
 const navbar = (
-    <div>
-    <h1>Fun facts about React</h1>
-    <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100K stars on GitHub</li>
-        <li>Is maintained by Facebook</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-</div>
+    <nav>
+        <h1>Bob's Bistro</h1>
+        <ul>
+            <li>Menu</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
 )
 
 ReactDOM.render(navbar, document.getElementById("root"))
